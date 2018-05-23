25e79bd1e2ba0ed6fa195a2c88bed28644233ff1
xooDJoox <37538836+xooDJoox@users.noreply.github.com>
2018-05-23 10:34:13 +1000

    小车移动

--- traffic_sim.py
+++ traffic_sim.py
@@ -15,6 +15,9 @@ class traffic_lights(tk.Tk,object):
         self.geometry("500x500")
         self.build_menu()
         self.Cross_street()
+        self.Car()
+        self.canvas.pack()
+        self.move_car()
 
     def Exit(self):
         self.destroy()
@@ -54,16 +57,38 @@ class traffic_lights(tk.Tk,object):
     
     def Cross_street(self):
         self.canvas=tk.Canvas(self,bg="white",height=500,width=500)
-        self.canvas.create_line(0,200,200,200)
-        self.canvas.create_line(200,0,200,200)
-        self.canvas.create_line(0,300,200,300)
-        self.canvas.create_line(300,0,300,200)
-        self.canvas.create_line(200,500,200,300)
-        self.canvas.create_line(500,200,300,200)
-        self.canvas.create_line(300,500,300,300)
-        self.canvas.create_line(500,300,300,300)
-        self.canvas.pack()
+        self.canvas.create_line(0,225,225,225)
+        self.canvas.create_line(225,0,225,225)
+        self.canvas.create_line(0,275,225,275)
+        self.canvas.create_line(275,0,275,225)
+        self.canvas.create_line(225,500,225,275)
+        self.canvas.create_line(500,225,275,225)
+        self.canvas.create_line(275,500,275,275)
+        self.canvas.create_line(500,275,275,275)
 
+    def Car(self):
+        self.x1=245
+        self.x2=255
+        self.y1=0
+        self.y2=10
+        self.x3=0
+        self.x4=10
+        self.y3=245
+        self.y4=255
+        self.Car_1 = self.canvas.create_rectangle(self.x1,self.y1,self.x2,self.y2,fill='black')
+
+    def move_car(self):
+        for self.vector in range(0,20):
+            self.canvas.coords(self.Car_1,(self.x1,self.y1,self.x2,self.y2))
+            self.canvas.coords(self.Car_2,(self.x3,self.y3,self.x4,self.y4))
+            self.update()
+            self.y1+=25
+            self.y2+=25
+            self.x3+=25
+            self.x4+=25
+            time.sleep(0.5) 
+
+    
 
 if __name__ == '__main__':
     env = traffic_lights()
