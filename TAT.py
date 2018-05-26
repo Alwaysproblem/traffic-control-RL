946e131f3ea2dba323cc37efd642ab6523b720f1
Prophet <reganyang0415@gmail.com>
2018-05-26 17:22:17 +1000

    update the car funtion

--- TAT.py
+++ TAT.py
@@ -1,2 +1,9 @@
-from traffic_sim import Car
+from traffic_sim import Vehicle
+import tkinter as tk
 
+C = tk.Canvas(bg = "white", height = 100, width = 100)
+
+a = Vehicle('left', C)
+
+print(a.x1)
+print(a.can_id)
